7ce2612d35862eb0debf9e2aeb582967b609c9d4
Daren Morreale <daren@givelegacy.com>
2025-02-04 17:48:41 -0500

    clean up

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // TODO: this file! :)
+const state = {
+  numbers: [],
+  total: 0,
+};
+
+const form = document.querySelector("form");
+const output = document.querySelector("output");
+const total = document.querySelector(".total");
