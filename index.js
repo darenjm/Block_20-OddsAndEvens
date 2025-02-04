f0dac7c8626dd12e04c48093d260cdde184ea468
Daren Morreale <daren@givelegacy.com>
2025-02-04 18:37:44 -0500

    updated

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const state = {
   total: 0,
 };
 
+const form = document.querySelector("form");
+const input = document.querySelector("#number");
+const output = document.querySelector("output");
 
-
+form.addEventListener("submit", addNumber);
