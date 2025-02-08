5dad501ac3e05dd104144cffe87a9cea1f04794e
Daren Morreale <daren@givelegacy.com>
2025-02-08 12:48:40 -0500

    added numbers array to output

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ const input = document.querySelector("#number");
 const output = document.querySelector("output");
 
 form.addEventListener("submit", addNumber);
+
+
+function renderNumbers(numberBank, element) {
+  //numberBank [1, 2, 3, 4] --> '1,2,3,4'
+  const commaNumberBank = numberBank.toString();
+  element.replaceChildren(commaNumberBank);
+}
+
+
+
+
+function addNumber(event) {
+  //prevent the page from refreshing
+  event.preventDefault();
+
+  //get the number from form
+  const theNumber = form.elements.number.value;
+
+  //add the number to the `number` array in state if the input is a number
+  if (parseInt(theNumber)) {
+    state.numbers.push(theNumber);
+
+    //replace the total div with the string values of what's in our `number` array
+    renderNumbers(state.numbers, output);
+  }
+
+  //clear the form's the input
+  form.elements.number.value = null;
+}
